bef3190a0bf284149f9d2b841082ef06f5895edd
tasty_fan14 <tastyfan14@gmail.com>
2026-08-20 11:30:26 +1000

    feat: add new icons

---
+++ src/shared/assets/icons/IOrderConfimation.tsx
@@ -0,0 +1,12 @@
+import { IconProps } from '../types'
+
+export default function IOrderConfirmation({ ...props }: IconProps) {
+    return (
+        <svg {...props} width="64" height="64" xmlns="http://www.w3.org/2000/svg">
+        <g fill="none" fillRule="evenodd">
+        <circle fill="currentColor" cx="32" cy="32" r="32"/>
+        <path stroke="currentColor" strokeWidth="4" d="m20.754 33.333 6.751 6.751 15.804-15.803"/>
+        </g>
+        </svg>
+    )
+}
---
+++ src/shared/assets/icons/IRecommendedCircle.tsx
@@ -0,0 +1,13 @@
+import { IconProps } from '../types'
+
+export default function IRecommendedCircle({ ...props }: IconProps) {
+    return (
+        <svg {...props} width="944" height="944" xmlns="http://www.w3.org/2000/svg">
+        <g stroke="currentColor" fill="none" fillRule="evenodd" opacity=".202">
+        <circle cx="472" cy="472" r="235.5"/>
+        <circle cx="472" cy="472" r="270.5"/>
+        <circle cx="472" cy="472" r="471.5"/>
+        </g>
+        </svg>
+    )
+}
