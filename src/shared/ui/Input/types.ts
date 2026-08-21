b13997541e62e62b48834a16bb7d79832ffbd418
tasty_fan14 <tastyfan14@gmail.com>
2026-08-21 14:09:40 +1000

    fix: improve types

--- src/shared/ui/Input/types.ts
+++ src/shared/ui/Input/types.ts
@@ -1,11 +1,8 @@
 export type InputProps = {
-    title: string
-
     rightEl?: React.ComponentType<React.SVGProps<SVGSVGElement>>
     leftEl?: React.ComponentType<React.SVGProps<SVGSVGElement>>
 
     className?: string
 
-    errorMessage: string | undefined
     errorId: string
 } & React.ComponentPropsWithoutRef<'input'>
