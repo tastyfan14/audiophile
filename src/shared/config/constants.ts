929e07225f362ce11db239ac99b3bdce93612b99
tasty_fan14 <tastyfan14@gmail.com>
2026-08-21 17:23:22 +1000

    feat: add new constants

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -1,3 +1,5 @@
+// tsx breakpoint
+
 export const BREAKPOINTS = {
     Mobile: 375,
     MobileIn: 327,
@@ -10,5 +12,46 @@ export const BREAKPOINTS = {
     PostMobile: 437, // Calculation formula: / 3, / 2, x 7 - 0.5
 } as const
 
+// checkout
+
 export const SHIPPING = 50
 export const VAR_RATE = 0.2
+
+// links
+
+export const SOCIAL_LINKS = {
+    Facebook: 'https://www.facebook.com',
+    Twitter: 'https://twitter.com',
+    Instagram: 'https://www.instagram.com',
+} as const
+
+// route
+
+const PRODUCTS = '/products'
+
+export const ROUTES = {
+    Home: {
+        route: '/',
+        label: 'HOME',
+    },
+    Product: {
+        route: PRODUCTS,
+        label: 'PRODUCT',
+    },
+    Headphones: {
+        route: `${PRODUCTS}/headphones`,
+        label: 'HEADPHONES',
+    },
+    Speakers: {
+        route: `${PRODUCTS}/speakers`,
+        label: 'SPEAKERS',
+    },
+    Earphones: {
+        route: `${PRODUCTS}/earphones`,
+        label: 'EARPHONES',
+    },
+    Checkout: {
+        route: '/checkout',
+        label: 'CHECKOUT',
+    },
+} as const
