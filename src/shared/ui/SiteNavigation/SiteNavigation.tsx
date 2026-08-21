71e0804f1d65c79fc6f8357de0edff324e5bd744
tasty_fan14 <tastyfan14@gmail.com>
2026-08-21 16:49:31 +1000

    feat: add footer&header navigation component

---
+++ src/shared/ui/SiteNavigation/SiteNavigation.tsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx'
+import cls from './SiteNavigation.module.scss'
+import { ROUTES } from '@/shared/config/constants'
+import Link from 'next/link'
+
+export default function SiteNavigation({ className }: { className?: string }) {
+    return (
+        <nav
+        className={clsx(cls.navigation, className)}
+        aria-label='Site navigation'
+        >
+            <li className={clsx(cls['navigation__li'])}>
+                <Link href={ROUTES.Home.route}>{ROUTES.Home.label}</Link>
+            </li>
+
+            <li className={clsx(cls['navigation__li'])}>
+                <Link href={ROUTES.Headphones.route}>{ROUTES.Headphones.label}</Link>
+            </li>
+
+            <li className={clsx(cls['navigation__li'])}>
+                <Link href={ROUTES.Speakers.route}>{ROUTES.Speakers.label}</Link>
+            </li>
+
+            <li className={clsx(cls['navigation__li'])}>
+                <Link href={ROUTES.Earphones.route}>{ROUTES.Earphones.label}</Link>
+            </li>
+        </nav>
+    )
+}
