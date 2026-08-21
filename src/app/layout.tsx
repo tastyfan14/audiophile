7cd64b240458f7044dff6376a938236b3534d220
tasty_fan14 <tastyfan14@gmail.com>
2026-08-21 17:26:49 +1000

    feat: add footer component in tree

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Manrope } from 'next/font/google'
 import '@/shared/styles/main.scss'
-import AppLayout from '@/shared/ui/layout/ui/AppLayout'
+import AppLayout from '@/shared/ui/Layout/ui/AppLayout'
+import Footer from '@/widgets/Footer/Footer'
 
 const ManropeFont = Manrope({
     variable: '--font-manrope',
@@ -23,6 +24,8 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
             <body>
                 <AppLayout>
                     {children}
+
+                    <Footer />
                 </AppLayout>
             </body>
         </html>
