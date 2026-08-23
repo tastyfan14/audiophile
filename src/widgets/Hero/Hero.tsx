9d98f428b6dfe5c41125d8d00ceb89ae0671fe61
tasty_fan14 <tastyfan14@gmail.com>
2026-08-23 14:48:48 +1000

    refactor: clear superfluous clsx, rename classes

--- src/widgets/Hero/Hero.tsx
+++ src/widgets/Hero/Hero.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import clsx from 'clsx'
 import cls from './Hero.module.scss'
 import Button from '@/shared/ui/Button'
 import Container from '@/shared/ui/Layout/ui/Container'
@@ -8,11 +7,11 @@ import { ROUTES } from '@/shared/config/constants'
 
 export default function Hero() {
     return (
-        <section className={clsx(cls['hero'])}>
-            <Container className={clsx(cls['hero__container'])}>
-                <p className={clsx(cls['hero__overview--sub'])}>NEW PRODUCT</p>
-                <h1 className={clsx(cls['hero__overview--title'])}>XX99 Mark II Headphones</h1>
-                <p className={clsx(cls['hero__overview--desc'])}>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+        <section className={cls.hero}>
+            <Container className={cls['hero__container']}>
+                <p className={cls['hero__sub']}>NEW PRODUCT</p>
+                <h1 className={cls['hero__title']}>XX99 Mark II Headphones</h1>
+                <p className={cls['hero__desc']}>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
 
                 <Button
                 as='link'
