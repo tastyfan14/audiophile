cd182dda82026e932d357c4cdd65d042f8c9b69b
tasty_fan14 <tastyfan14@gmail.com>
2026-08-24 03:05:04 +1000

    refactor: add classname + clsx

--- src/shared/ui/Counter/Counter.tsx
+++ src/shared/ui/Counter/Counter.tsx
@@ -1,12 +1,13 @@
 import type { CounterProps } from './types'
 import cls from './Counter.module.scss'
 import Button from '../Button'
+import clsx from 'clsx'
 
-export default function Counter({ value, onChange }: CounterProps) {
+export default function Counter({ value, onChange, className }: CounterProps) {
     const decrement = () => onChange(Math.max(1, value - 1))
     const increment = () => onChange(value + 1)
     return (
-        <div className={cls.counter}>
+        <div className={clsx(cls.counter, className)}>
             <Button
             variant='additional'
             className={cls['counter__button']}
