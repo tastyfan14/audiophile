faa8eaaf219366aa4d94d4ce1086da0ee317d210
tasty_fan14 <tastyfan14@gmail.com>
2026-09-11 05:05:53 +1000

    feat: add sorting component for product cards

---
+++ src/widgets/Products/ui/List/List.tsx
@@ -0,0 +1,23 @@
+import ProductCard from '@/entities/product/ui/ProductCard'
+import cls from './List.module.scss'
+import type { Product } from '@/entities/product/model/types'
+
+type Props = {
+    products: Product[]
+}
+
+export default async function List({ products }: Props) {
+    return (
+        <ul className={cls['list']}>
+            {products.map((product, index) => {
+                return (
+                    <ProductCard
+                    key={product.id}
+                    product={product}
+                    reverse={index % 2 !== 0}
+                    />
+                )
+            })}
+        </ul>
+    )
+}
