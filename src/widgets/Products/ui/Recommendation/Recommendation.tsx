48b1c28b69bc396c18168e2ad2d5cd7b6d012a30
tasty_fan14 <tastyfan14@gmail.com>
2026-09-13 03:22:27 +1000

    feat: add recommended product section

---
+++ src/widgets/Products/ui/Recommendation/Recommendation.tsx
@@ -0,0 +1,58 @@
+import type { ProductRecommendation } from '@/entities/product/model/types'
+import cls from './Recommendation.module.scss'
+import ResponsiveImage from '@/shared/ui/ResponsiveImage'
+import Button from '@/shared/ui/Button'
+import { ROUTES } from '@/shared/config/constants'
+
+type Props = {
+    recommendations: ProductRecommendation[]
+}
+
+export default function Recommendation({ recommendations }: Props) {
+    return (
+        <section
+        className={cls.recommendation}
+        aria-labelledby='recommendations-title'
+        >
+            <h2
+            id='recommendations-title' className={cls['recommendation__title']}
+            >
+                You may also like
+            </h2>
+
+            <ul
+            className={cls['recommendation__cards']}
+            >
+                {recommendations.map((recommendation) => {
+                    return (
+                        <li
+                        key={recommendation.id}
+                        className={cls['recommendation__card']}
+                        >
+                            <ResponsiveImage
+                            mobile={`/images/mobile/products/image-${recommendation.image}`}
+                            laptop={`/images/laptop/products/image-${recommendation.image}`}
+                            desktop={`/images/desktop/products/image-${recommendation.image}`}
+                            alt=''
+                            aria-hidden='true'
+                            className={cls['recommendation__card--picture']}
+                            loading='lazy'
+                            />
+
+                            <h3 className={cls['recommendation__card--title']}>{recommendation.title}</h3>
+
+                            <Button
+                            as='link'
+                            href={`${ROUTES[recommendation.category as keyof typeof ROUTES].route}/${recommendation.slug}`}
+                            variant='primary'
+                            className={cls['recommendation__card--button']}
+                            >
+                                See Product
+                            </Button>
+                        </li>
+                    )
+                }).slice(0, 3)}
+            </ul>
+        </section>
+    )
+}
