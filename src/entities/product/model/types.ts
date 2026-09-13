d12309ee88a6f08e347e9754f38313c771b75420
tasty_fan14 <tastyfan14@gmail.com>
2026-09-13 20:40:27 +1000

    refactor: improve types

--- src/entities/product/model/types.ts
+++ src/entities/product/model/types.ts
@@ -1,15 +1,78 @@
-export interface Product {
+import { PRODUCT_SHOWCASE_VARIANTS } from './config'
+
+export type ProductBadge = {
+    id: string
+    label: string
+}
+
+export type ProductFeature = {
+    id: string
+    desc: string
+}
+
+export type ProductInclude = {
+    id: string
+    quantity: number
+    title: string
+}
+
+export type ProductGallery = {
+    id: string
+    mobile: string
+    laptop: string
+    desktop: string
+    alt: string
+}
+
+export type ProductRecommendation = {
     id: string
     slug: string
-    category: string[] //
-    badges: string[] //
+    category: string
+    image: string
+    title: string
+}
+
+export interface Product {
+    id: string
+
+    slug: string
+    category: string
+
+    badges: ProductBadge[]
+
     image: string
     title: string
     desc: string
     price: number
-    features: string[] //
-    includes: string[] //
-    gallery: string[] //
-    recommendations: string[] //
+
+    features: ProductFeature[]
+    includes: ProductInclude[]
+    gallery: ProductGallery[]
+    recommendations: ProductRecommendation[]
+
     stock: number
 }
+
+// Home showcase
+
+export type ProductShowcaseVariants = (typeof PRODUCT_SHOWCASE_VARIANTS)[keyof typeof PRODUCT_SHOWCASE_VARIANTS]
+
+type ProductShowcaseBase = {
+    id: string
+    slug: string
+    category: string
+    title: string
+    image: string
+}
+
+export type ProductShowcase =
+    | (ProductShowcaseBase & {
+        variant: typeof PRODUCT_SHOWCASE_VARIANTS.accent
+        desc: string
+    })
+    | (ProductShowcaseBase & {
+        variant: typeof PRODUCT_SHOWCASE_VARIANTS.default
+    })
+    | (ProductShowcaseBase & {
+        variant: typeof PRODUCT_SHOWCASE_VARIANTS.double
+    })
