85c8089362e8fa85222b70f8e1c0e7942113db1c
tasty_fan14 <tastyfan14@gmail.com>
2026-09-13 20:44:30 +1000

    fix: add class for different style setting

--- src/shared/ui/Overlay/Overlay.tsx
+++ src/shared/ui/Overlay/Overlay.tsx
@@ -1,15 +1,18 @@
 import type { OverlayProps } from './types'
 import clsx from 'clsx'
 import cls from './Overlay.module.scss'
+import { useLockBodyScroll } from '@/shared/lib/useLockBodyScroll'
 
 export default function Overlay({ isOpen, onClose, className, coverage, children }: OverlayProps) {
+    useLockBodyScroll(isOpen)
+
     if (!isOpen) return null
 
     return (
         <div className={clsx(cls.overlay, cls[`overlay-${coverage}`])}>
             <button
             type='button'
-            className={cls['overlay__backdrop']}
+            className={clsx(cls['overlay__backdrop'], cls[`overlay-${coverage}__backdrop`])}
             aria-label='Close'
             onClick={onClose}
             />
