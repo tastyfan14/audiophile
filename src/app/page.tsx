2c200e03b451881666d7443ea4ca6cf39209be7a
tasty_fan14 <tastyfan14@gmail.com>
2026-09-13 20:52:07 +1000

    feat: add showcase component in tree, rename func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,17 @@ import MainLayout from '@/shared/ui/Layout/ui/MainLayout'
 import Hero from '@/widgets/Hero/Hero'
 import Banner from '@/widgets/Banner/Banner'
 import CategoryNavigation from '@/widgets/CategoryNavigation/CategoryNavigation'
+import Showcase from '@/widgets/Products/ui/Showcase/Showcase'
 
-export default function Home() {
+export default function HomePage() {
     return (
         <PageWrapper>
             <Hero />
 
             <MainLayout>
                 <CategoryNavigation variant='page' />
+
+                <Showcase />
 
                 <Banner />
             </MainLayout>
