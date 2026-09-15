c76d75970986b7e700edb47a135ab5a9e428754e
tasty_fan14 <tastyfan14@gmail.com>
2026-09-15 13:12:58 +1000

    feat: add properties for new feat - variant

--- src/shared/ui/Counter/Counter.tsx
+++ src/shared/ui/Counter/Counter.tsx
@@ -3,22 +3,22 @@ import cls from './Counter.module.scss'
 import Button from '../Button'
 import clsx from 'clsx'
 
-export default function Counter({ value, onChange, className }: CounterProps) {
+export default function Counter({ value, onChange, variant, className }: CounterProps) {
     const decrement = () => onChange(Math.max(0, value - 1))
     const increment = () => onChange(value + 1)
     return (
-        <div className={clsx(cls.counter, className)}>
+        <div className={clsx(cls.counter, cls[`counter-${variant}`], className)}>
             <Button
             variant='additional'
             className={cls['counter__button']}
-            disabled={value === 0}
+            disabled={variant === 'cart' ? value === 0 : value === 1}
             aria-label='Decrease quantity'
             onClick={decrement}
             >
                 -
             </Button>
 
-            <span aria-live='polite' className={cls['counter__value']}>{value}</span>
+            <span aria-live='polite' className={cls[`counter-${variant}__value`]}>{value}</span>
 
             <Button
             variant='additional'
