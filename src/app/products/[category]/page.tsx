e073263da21e83dfe33af3ba87848b239e2db8e3
tasty_fan14 <tastyfan14@gmail.com>
2026-09-16 11:18:28 +1000

    feat: add category dynamic page

--- src/app/products/[category]/page.tsx
+++ src/app/products/[category]/page.tsx
@@ -1,6 +1,11 @@
+import { getProducts } from '@/entities/product/api/getProducts'
 import { ROUTES } from '@/shared/config/constants'
+import MainLayout from '@/shared/ui/Layout/ui/MainLayout'
 import PageWrapper from '@/shared/ui/Layout/ui/PageWrapper'
+import Banner from '@/widgets/Banner/Banner'
+import CategoryNavigation from '@/widgets/CategoryNavigation/CategoryNavigation'
 import HeroPage from '@/widgets/HeroPage/HeroPage'
+import ProductList from '@/widgets/Products/ui/List/List'
 import { notFound } from 'next/navigation'
 
 interface CategoryProps {
@@ -9,19 +14,28 @@ interface CategoryProps {
     }>
 }
 
-export default async function Category({ params }: CategoryProps) {
+export default async function CategoryPage({ params }: CategoryProps) {
     const { category } = await params
 
-    const formattedCategory = category.charAt(0).toUpperCase() + category.slice(1)
-
-    const currentCategory = ROUTES[formattedCategory as keyof typeof ROUTES]
+    const currentCategory = ROUTES[category as keyof typeof ROUTES]
 
     if (!currentCategory) {
         notFound()
     }
+
+    const products = await getProducts(category)
+
     return (
         <PageWrapper>
             <HeroPage title={currentCategory.label} />
+
+            <MainLayout>
+                <ProductList products={products} />
+
+                <CategoryNavigation variant='page' />
+
+                <Banner />
+            </MainLayout>
         </PageWrapper>
     )
 }
