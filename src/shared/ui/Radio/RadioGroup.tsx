ee10e7d60dbecda4704adbecf75f7b5e5501dcb9
tasty_fan14 <tastyfan14@gmail.com>
2026-09-16 11:25:27 +1000

    fix: restructure for proper operation and correct semantics

--- src/shared/ui/Radio/RadioGroup.tsx
+++ src/shared/ui/Radio/RadioGroup.tsx
@@ -1,17 +1,30 @@
 import type { RadioGroupProps } from './types'
 import cls from './RadioGroup.module.scss'
+import clsx from 'clsx'
 
-export default function RadioGroup<T extends({ id: string })>({ name, value, onChange, options, renderCard }: RadioGroupProps<T>) {
+export default function RadioGroup<T extends({ id: string })>({ title, name, value, onChange, options, renderCard, errorId, errorMessage, className }: RadioGroupProps<T>) {
+    const error = errorMessage && errorMessage !== 'none'
+    
     return (
-        <div className={cls['radio-group']}>
-            {options.map((option) => 
-                renderCard({
-                    option,
-                    name,
-                    checked: value === option.id,
-                    onChange: () => onChange(option.id)
-                })
-            )}
-        </div>
+        <fieldset className={clsx(cls['radio-group'], className)}>
+            <div className={cls['radio-group-overview']}>
+                <legend className={clsx(cls['radio-group-overview__title'], error && cls['radio-group-overview__title--error'])}>{title}</legend>
+
+                {error && (
+                    <span id={`error-${errorId}`} className={cls['radio-group-overview__error']}>{errorMessage}</span>
+                )}
+            </div>
+
+            <div className={cls['radio-group__options']}>
+                {options.map((option) => 
+                    renderCard({
+                        option,
+                        name,
+                        checked: value === option.id,
+                        onChange: () => onChange(option.id)
+                    })
+                )}
+            </div>
+        </fieldset>
     )
 }
