f12624ad5a8a7c7c1544f8058256d7ac1039aff3
tasty_fan14 <tastyfan14@gmail.com>
2026-09-18 09:08:21 +1000

    feat: add states and rendering in conjunction with tanstack

--- apps/web/src/widgets/Products/ui/List/List.tsx
+++ apps/web/src/widgets/Products/ui/List/List.tsx
@@ -1,11 +1,33 @@
+'use client'
+
 import ProductCard from '@/entities/product/ui/ProductCard'
 import cls from './List.module.scss'
 import type { ProductListProps } from '../../model/types'
+import type { ProductDto } from '@audiophile/shared'
+import { useProducts } from '@/entities/product/model/useProducts'
 
-export default async function List({ products }: ProductListProps) {
+export default function List({ category }: ProductListProps) {
+    const {
+        data: products = [],
+        isLoading,
+        isError,
+    } = useProducts(category)
+
+    if (isLoading) {
+        return <p className={cls['list__state']}>Loading...</p>
+    }
+
+    if (isError) {
+        return <p className={cls['list__state']}>Failed to load products.</p>
+    }
+
+    if (!products?.length) {
+        return <p className={cls['list__state']}>No products found.</p>
+    }
+
     return (
         <ul className={cls.list} aria-label='Product list'>
-            {products.map((product, index) => {
+            {products.map((product: ProductDto, index: number) => {
                 return (
                     <ProductCard
                     key={product.id}
