315db1bc99cdc9e8bb33acdc6f2c546cd28062eb
tasty_fan14 <tastyfan14@gmail.com>
2026-09-18 07:14:49 +1000

    feat: restructure component for the added tanstack

--- apps/web/src/widgets/Cart/Cart.tsx
+++ apps/web/src/widgets/Cart/Cart.tsx
@@ -14,15 +14,17 @@ import { useEscapeKey } from '@/shared/lib/useEscapeKey'
 import { calculateTotal } from '@/entities/cart/lib/calculations'
 import { formatPrice } from '@/shared/lib/formatPrice'
 
-export default function Cart({ isSyncing, isOpen, onClose, className }: CartProps) {
+export default function Cart({ stock, isLoading, isFetching, isError, isOpen, onClose, className }: CartProps) {
     useEscapeKey({ isOpen, onClose })
 
     const items = useCartStore(state => state.items)
     const total = calculateTotal(items)
 
+    const isWaiting = isLoading || isFetching
+
     return (
         <FocusTrap
-        active={isOpen && !isSyncing}
+        active={isOpen && !isWaiting && !isError}
         focusTrapOptions={{
             returnFocusOnDeactivate: true,
             clickOutsideDeactivates: true,
@@ -30,68 +32,70 @@ export default function Cart({ isSyncing, isOpen, onClose, className }: CartProp
         >
             {/* Checking the availability of items in the cart */}
             {items.length === 0
-                ? (
-                    <div
-                    id='cart'
-                    className={clsx(cls.cart, cls['cart-empty'], className)}
-                    role='dialog'
-                    aria-modal='true'
+            ? (
+                <div
+                id='cart'
+                className={clsx(cls.cart, cls['cart-empty'], className)}
+                role='dialog'
+                aria-modal='true'
+                >
+                    <h2 className={cls['cart-empty__title']}>Your cart is empty</h2>
+
+                    <p className={cls['cart-empty__desc']}>Don’t wait, start shopping now.</p>
+
+                    <Button
+                    as='link'
+                    href={ROUTES.products.route}
+                    variant='secondary'
+                    className={cls['cart-empty__button']}
+                    onClick={onClose}
                     >
-                        <h2 className={cls['cart-empty__title']}>Your cart is empty</h2>
+                        Shop Now
+                    </Button>
+                </div>
+            ) : (
+                <div
+                id='cart'
+                className={clsx(cls.cart, cls['cart-content'], isWaiting && cls['cart-content--loading'])}
+                role='dialog'
+                aria-modal='true'
+                >
+                    {isWaiting ? (
+                        <Spinner />
+                    ) : isError ? (
+                        <p className={cls['cart-content--error']}>Failed to update product availability.</p>
+                    ) : (
+                        <>
+                            {/* There's a product counter and a full cleaning button */}
+                            <div className={cls['cart-content__interactive']}>
+                                <h2 className={cls['cart-content__interactive--title']}>Cart ({items.length})</h2>
+                                
+                                <RemoveFromCart className={cls['cart-content__interactive--button']} />
+                            </div>
 
-                        <p className={cls['cart-empty__desc']}>Don’t wait, start shopping now.</p>
+                            {/* Here are the products themselves */}
+                            <SelectedProducts variant='cart' />
 
-                        <Button
-                        as='link'
-                        href={ROUTES.products.route}
-                        variant='secondary'
-                        className={cls['cart-empty__button']}
-                        onClick={onClose}
-                        >
-                            Shop Now
-                        </Button>
-                    </div>
-                ) : (
-                    <div
-                    id='cart'
-                    className={clsx(cls.cart, cls['cart-content'], isSyncing && cls['cart-content--loading'])}
-                    role='dialog'
-                    aria-modal='true'
-                    >
-                        {isSyncing ? (
-                            <Spinner />
-                        ) : (
-                            <>
-                                {/* There's a product counter and a full cleaning button */}
-                                <div className={cls['cart-content__interactive']}>
-                                    <h2 className={cls['cart-content__interactive--title']}>Cart ({items.length})</h2>
-                                    
-                                    <RemoveFromCart className={cls['cart-content__interactive--button']} />
-                                </div>
+                            {/* Cta */}
+                            <div className={cls['cart-content__cta']}>
+                                <h2 className={cls['cart-content__cta--title']}>TOTAL</h2>
+                                <h3 className={cls['cart-content__cta--price']}>$ {formatPrice(total)}</h3>
+                            </div>
 
-                                {/* Here are the products themselves */}
-                                <SelectedProducts variant='cart' />
-
-                                {/* Cta */}
-                                <div className={cls['cart-content__cta']}>
-                                    <h2 className={cls['cart-content__cta--title']}>TOTAL</h2>
-                                    <h3 className={cls['cart-content__cta--price']}>$ {formatPrice(total)}</h3>
-                                </div>
-
-                                {/* Go to checkout */}
-                                <Button
-                                as='link'
-                                href={ROUTES.checkout.route}
-                                variant='primary'
-                                className={cls['cart-content__button']}
-                                onClick={onClose}
-                                >
-                                    {ROUTES.checkout.label}
-                                </Button>
-                            </>
-                        )}
-                    </div>
-                )
+                            {/* Go to checkout */}
+                            <Button
+                            as='link'
+                            href={ROUTES.checkout.route}
+                            variant='primary'
+                            className={cls['cart-content__button']}
+                            onClick={onClose}
+                            >
+                                {ROUTES.checkout.label}
+                            </Button>
+                        </>
+                    )}
+                </div>
+            )
             }
         </FocusTrap>
     )
