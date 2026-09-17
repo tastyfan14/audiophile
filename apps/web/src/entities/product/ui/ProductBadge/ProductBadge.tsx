080a9d75d2479147b0f375761b2e4f96d0319621
tasty_fan14 <tastyfan14@gmail.com>
2026-09-17 19:32:31 +1000

    feat: add classname props

--- apps/web/src/entities/product/ui/ProductBadge/ProductBadge.tsx
+++ apps/web/src/entities/product/ui/ProductBadge/ProductBadge.tsx
@@ -1,9 +1,10 @@
+import clsx from 'clsx'
 import type { ProductBadgeProps } from '../../model/types'
 import cls from './ProductBadge.module.scss'
 
-export default function ProductBadge({ badges, stock }: ProductBadgeProps) {
+export default function ProductBadge({ badges, stock, className }: ProductBadgeProps) {
     return (
-        <ul className={cls['product-badge']}>
+        <ul className={clsx(cls['product-badge'], className)}>
             {stock <= 50
             ? (
                 <span className={cls['product-badge__stock']}>Only {stock} left</span>
