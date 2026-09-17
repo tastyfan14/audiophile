14c8330d2e46a57207f7ddeeb8cc7b1e33539c12
tasty_fan14 <tastyfan14@gmail.com>
2026-09-18 09:12:58 +1000

    refactor: add new types path

--- apps/web/src/entities/checkout/api/postCreateOrder.ts
+++ apps/web/src/entities/checkout/api/postCreateOrder.ts
@@ -1,5 +1,5 @@
-import type { CheckoutCreateOrderDto, CheckoutOrderResponse } from './types'
 import { api } from '@/shared/api/api'
+import type { CheckoutCreateOrderDto, CheckoutOrderResponse } from '@audiophile/shared'
 
 export async function postCreateOrder(data: CheckoutCreateOrderDto): Promise<CheckoutOrderResponse> {
     const { data: order } = await api.post<CheckoutOrderResponse>('/api/checkout/create', data)
