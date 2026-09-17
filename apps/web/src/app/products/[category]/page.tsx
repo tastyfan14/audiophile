9cc270382c89d79401b798faed0b7bfc2dfc6882
tasty_fan14 <tastyfan14@gmail.com>
2026-09-18 09:09:58 +1000

    feat: add tanstack

--- apps/web/src/app/products/[category]/page.tsx
+++ apps/web/src/app/products/[category]/page.tsx
@@ -1,11 +1,13 @@
 import { getProducts } from '@/entities/product/api/getProducts'
 import { ROUTES } from '@/shared/config/constants'
+import { getQueryClient } from '@/shared/lib/getQueryClient'
 import MainLayout from '@/shared/ui/Layout/ui/MainLayout'
 import PageWrapper from '@/shared/ui/Layout/ui/PageWrapper'
 import Banner from '@/widgets/Banner/Banner'
 import CategoryNavigation from '@/widgets/CategoryNavigation/CategoryNavigation'
 import HeroPage from '@/widgets/HeroPage/HeroPage'
 import ProductList from '@/widgets/Products/ui/List/List'
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
 import { notFound } from 'next/navigation'
 
 interface CategoryProps {
@@ -23,14 +25,21 @@ export default async function CategoryPage({ params }: CategoryProps) {
         notFound()
     }
 
-    const products = await getProducts(category)
+    const queryClient = getQueryClient()
+
+    await queryClient.prefetchQuery({
+        queryKey: ['products', category],
+        queryFn: () => getProducts(category),
+    })
 
     return (
         <PageWrapper>
             <HeroPage title={currentCategory.label} />
 
             <MainLayout>
-                <ProductList products={products} />
+                <HydrationBoundary state={dehydrate(queryClient)}>
+                    <ProductList category={category} />
+                </HydrationBoundary>
 
                 <CategoryNavigation variant='page' />
 
