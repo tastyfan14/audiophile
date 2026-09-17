ed94117215f6485b10570c17a25f94d7535b213b
tasty_fan14 <tastyfan14@gmail.com>
2026-09-17 20:56:21 +1000

    refactor: move types to model inside widgets, cleanup code

--- apps/web/src/widgets/Products/ui/Feature/Feature.tsx
+++ apps/web/src/widgets/Products/ui/Feature/Feature.tsx
@@ -1,15 +1,19 @@
-import clsx from 'clsx'
 import cls from './Feature.module.scss'
 import type { ProductFeatureProps } from '../../model/types'
 
-export default function Feature({ features, className }: ProductFeatureProps) {
+export default function Feature({ features }: ProductFeatureProps) {
     return (
-        <div className={clsx(cls['feature'], className)}>
-            <h2 className={cls['feature__title']}>FEATURES</h2>
+        <div className={cls.feature}>
+            <h2 className={cls['feature__title']}>Features</h2>
 
             {features.map((feature) => {
                 return (
-                    <p key={feature.id} className={cls['feature__desc']}>{feature.desc}</p>
+                    <p
+                    key={feature.id}
+                    className={cls['feature__desc']}
+                    >
+                        {feature.desc}
+                    </p>
                 )
             })}
         </div>
