a489df06da4eb4130ebae75ebffda3d60f733d56
tasty_fan14 <tastyfan14@gmail.com>
2026-09-17 20:52:34 +1000

    refactor: add /features components

--- apps/web/src/widgets/Products/ui/Page/Page.tsx
+++ apps/web/src/widgets/Products/ui/Page/Page.tsx
@@ -2,35 +2,22 @@
 
 import { useState } from 'react'
 import cls from './Page.module.scss'
-import { useCartStore } from '@/entities/cart/model/store'
 import ResponsiveImage from '@/shared/ui/ResponsiveImage'
-import Button from '@/shared/ui/Button'
 import Counter from '@/shared/ui/Counter'
+import BackToPreviousPage from '@/features/BackNavigation/ui/BackToPreviousPage'
+import AddToCart from '@/features/AddToCart/ui/AddToCart'
 import ProductBadge from '@/entities/product/ui/ProductBadge'
 import ProductFeature from '../Feature/Feature'
 import ProductGallery from '../Gallery/Gallery'
 import ProductInclude from '../Include/Include'
-import { useRouter } from 'next/navigation'
 import type { ProductPageProps } from '../../model/types'
 
 export default function Page({ product }: ProductPageProps) {
     const [quantity, setQuantity] = useState(1)
 
-    const addItem = useCartStore(state => state.addItem)
-
-    const outOfStock = product.stock === 0
-
-    const router = useRouter()
-
     return (
         <section className={cls.page}>
-            <Button
-            variant='additional'
-            className={cls['page__back']}
-            onClick={() => router.back()}
-            >
-                Go Back
-            </Button>
+            <BackToPreviousPage className={cls['page__back']} />
 
             <div className={cls['page-content']}>
                 <div className={cls['page-overview']}>
@@ -62,27 +49,14 @@ export default function Page({ product }: ProductPageProps) {
                         value={quantity}
                         onChange={setQuantity}
                         variant='page'
-                        className={outOfStock ? cls['page-overview__interactive--disabled'] : undefined}
                         />
 
-                        <Button
-                        variant='primary'
-                        className={outOfStock ? cls['page-overview__interactive--disabled'] : undefined}
-                        onClick={() => addItem({
-                            id: product.id,
-                            slug: product.slug,
-                            category: product.category,
-                            image: product.image,
-                            shortTitle: product.shortTitle,
-                            price: product.price,
-                            stock: product.stock,
-
-                            quantity: quantity,
-                        })}
-                        disabled={outOfStock ? true : false}
-                        >
-                            Add to Cart
-                        </Button>
+                        <AddToCart
+                        product={product}
+                        currentStock={product.stock}
+                        currentQuantity={quantity}
+                        className={cls['page-overview__interactive--disabled']}
+                        />
                     </div>
                 </div>
 
