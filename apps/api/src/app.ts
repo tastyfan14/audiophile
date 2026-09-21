823035ddd604d87c02b319ad6d5206fb78e45bbf
tasty_fan14 <tastyfan14@gmail.com>
2026-09-21 17:08:43 +1000

    feat: add sentry

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import * as Sentry from '@sentry/node'
 import ProductRouter from './modules/products/products.routes.js'
 import CheckoutRouter from './modules/orders/orders.routes.js'
 
@@ -16,5 +17,7 @@ app.use(express.json())
 app.use(ProductRouter)
 
 app.use(CheckoutRouter)
+
+Sentry.setupExpressErrorHandler(app)
 
 export default app
