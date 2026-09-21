cad82a819cd877b09d13311e2b277fb670699e8d
tasty_fan14 <tastyfan14@gmail.com>
2026-09-21 17:10:07 +1000

    chore: add client config for sentry

---
+++ apps/web/instrumentation-client.ts
@@ -0,0 +1,10 @@
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+    tracesSampleRate: 1.0,
+})
+
+export const onRouterTransitionStart =
+    Sentry.captureRouterTransitionStart
