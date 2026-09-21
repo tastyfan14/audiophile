484876f2438c96b9002ab720594eefd0470716ba
tasty_fan14 <tastyfan14@gmail.com>
2026-09-21 10:53:50 +1000

    fix: add pick for product

--- apps/web/src/features/AddToCart/model/types.ts
+++ apps/web/src/features/AddToCart/model/types.ts
@@ -1,7 +1,7 @@
 import type { Product } from '@/entities/product/model/types'
 
 export type AddToCart = {
-    product: Product
+    product: Pick<Product, 'id' | 'slug' | 'category' | 'image' | 'shortTitle' | 'price' | 'stock'>
     currentStock: number
     currentQuantity: number
     className?: string
