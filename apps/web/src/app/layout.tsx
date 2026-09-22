602d0eb684ee80e943f1d3644805aa27a956d72a
tasty_fan14 <tastyfan14@gmail.com>
2026-09-22 15:37:15 +1000

    feat: add metadata (prod.)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Manrope } from 'next/font/google'
 import '@/shared/styles/main.scss'
 import AppLayout from '@/shared/ui/Layout/ui/AppLayout'
 import QueryProvider from '@/shared/providers/QueryProvider'
+import { SITE_URL } from '@/shared/config/constants'
 
 const ManropeFont = Manrope({
     variable: '--font-manrope',
@@ -16,9 +17,48 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
+    metadataBase: new URL(SITE_URL),
+
     title: 'Audiophile',
     description: 'Audiophile e-commerce website created by tasty_fan14.',
-};
+
+    alternates: {
+        canonical: '/',
+    },
+
+    openGraph: {
+        title: 'Audiophile',
+        description: 'Audiophile e-commerce website created by tasty_fan14.',
+        url: SITE_URL,
+        siteName: 'Audiophile',
+        locale: 'en_US',
+        type: 'website',
+        images: [
+            {
+                url: '/preview.webp',
+                width: 800,
+                height: 600,
+                alt: 'Audiophile e-commerce website created by tasty_fan14.',
+            },
+        ],
+    },
+
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Audiophile',
+        description: 'Audiophile e-commerce website created by tasty_fan14.',
+        images: ['/preview.webp'],
+    },
+
+    robots: {
+        index: true,
+        follow: true,
+    },
+
+    icons: {
+        icon: '/favicon.ico',
+    },
+}
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
     return (
